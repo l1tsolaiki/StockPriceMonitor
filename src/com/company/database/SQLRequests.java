f441bedbd749217afe1c7c03c3591d3e1fc2adfc
kostya <atrd.konstantin@gmail.com>
2020-10-20 18:31:21 +0300

    Fixed rookie mistake

--- src/com/company/database/SQLRequests.java
+++
@@ -1,7 +0,0 @@
-package com.company.database;
-
-public class SQLRequests {
-    public static final String updateUserID = "INSERT INTO ChatIDs (Username, ChatID, Name) VALUES (?,?,?) ON DUPLICATE KEY UPDATE Username=?, Name=?";
-    public static final String updateSubscriptions = "INSERT INTO Subscriptions (ChatID, Subscriptions) VALUES (?, ?) ON DUPLICATE KEY UPDATE Subscriptions=?";
-    public static final String querySubscriptions = "FROM Subscriptions SELECT * WHERE ChatID=?";
-}
